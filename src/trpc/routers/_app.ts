9e917af176c95a74a083d57650e2e595a50d8eb0
sainvishaa <sainivishaa@gmail.com>
2026-03-24 16:14:50 +0530

    feat: integrate Polar SDK for subscription management and authentication

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,10 @@
 import { inngest } from '@/inngest/client';
-import {baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import {baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 
 
 export const appRouter = createTRPCRouter({
-  testAi: baseProcedure.mutation(async ()=> {
+  testAi: premiumProcedure.mutation(async ()=> {
     await inngest.send({
       name: "execute/ai",
     });
